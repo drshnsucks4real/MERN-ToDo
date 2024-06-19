1f11b451dc82b336eefb2fa5bf6c35d9fd94d92d
Darshan-Bhangale <144256690+drshnsucks4real@users.noreply.github.com>
2024-06-19 13:31:26 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import CustomForm from "./components/Form";
+import NavBar from "./components/NavBar";
+
+let initialState = {
+  title: '',
+  description: ''
+}
 
 function App() {
+  const [data, setData] = React.useState(initialState);
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NavBar />
+      <CustomForm handleChange={handleChange} values={data} />
     </div>
   );
 }
